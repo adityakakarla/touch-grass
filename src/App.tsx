381bf889287c505840d746f2fa1973ada615af89
adityakakarla <85147239+adityakakarla@users.noreply.github.com>
2023-06-25 15:54:18 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MyButton from "./components/MyButton";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center my-4 text-title">Touch Grass</h1>
+      </div>
+      <MyButton />
+    </>
+  );
+}
+
+export default App;
